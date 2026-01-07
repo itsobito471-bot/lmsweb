0155c0a83d208d4928930def500efa381c477f6c
ADIL <anwaradil295@gmail.com>
2026-01-07 23:41:03 +0530

    refactor: relocate landing page components and introduce dedicated admin and user modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { HeaderComponent } from './components/header/header.component';
-import { HeroComponent } from './components/hero/hero.component';
-import { StatsComponent } from './components/stats/stats.component';
-import { TrendingCoursesComponent } from './components/trending-courses/trending-courses.component';
-import { LearningCategoriesComponent } from './components/learning-categories/learning-categories.component';
-import { InstructorBannerComponent } from './components/instructor-banner/instructor-banner.component';
-import { TestimonialsComponent } from './components/testimonials/testimonials.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { LandingPageComponent } from './pages/landingPage/components/landing-page/landing-page.component';
+import { HeaderComponent } from './pages/landingPage/components/header/header.component';
+import { HeroComponent } from './pages/landingPage/components/hero/hero.component';
+import { StatsComponent } from './pages/landingPage/components/stats/stats.component';
+import { TrendingCoursesComponent } from './pages/landingPage/components/trending-courses/trending-courses.component';
+import { LearningCategoriesComponent } from './pages/landingPage/components/learning-categories/learning-categories.component';
+import { InstructorBannerComponent } from './pages/landingPage/components/instructor-banner/instructor-banner.component';
+import { TestimonialsComponent } from './pages/landingPage/components/testimonials/testimonials.component';
+import { FooterComponent } from './pages/landingPage/components/footer/footer.component';
 
 @NgModule({
   declarations: [
